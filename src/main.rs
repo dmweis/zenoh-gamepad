725c6b7ff2a80430f44145267c49afc3ab50b7ca
David Weis <dweis7@gmail.com>
2024-06-22 21:13:18 +0100

    Handle ctrlc and clean up

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
-use chrono::prelude::{DateTime, Utc};
+mod messages;
+
+use anyhow::Context;
 use clap::Parser;
+use std::{
+    collections::HashMap,
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    },
+    time::Duration,
+};
+
 use gilrs::GilrsBuilder;
-use schemars::{schema_for, JsonSchema};
-use serde::{Deserialize, Serialize};
-use std::collections::{BTreeMap, HashMap};
-use std::hash::Hash;
-use std::time::Duration;
+use schemars::schema_for;
 use thiserror::Error;
 use tracing::*;
-use zenoh::config::Config;
-use zenoh::prelude::r#async::*;
+use zenoh::{config::Config, prelude::r#async::*};
+
+use crate::messages::{Button, InputMessage};
 
 #[derive(Parser)]
 #[command(author, version)]
@@ -33,12 +41,33 @@ struct Args {
     /// Loop sleep time
     #[clap(short, long, default_value = "50")]
     sleep_ms: u64,
+
+    /// verbosity level
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    verbose: u8,
+}
+
+pub fn setup_tracing(verbosity_level: u8) {
+    let filter = match verbosity_level {
+        0 => tracing::level_filters::LevelFilter::INFO,
+        1 => tracing::level_filters::LevelFilter::DEBUG,
+        2 => tracing::level_filters::LevelFilter::TRACE,
+        _ => tracing::level_filters::LevelFilter::TRACE,
+    };
+
+    tracing_subscriber::fmt().with_max_level(filter).init();
+}
+
+#[derive(Error, Debug)]
+pub enum ErrorWrapper {
+    #[error("Zenoh error {0:?}")]
+    ZenohError(#[from] zenoh::Error),
 }
 
 #[tokio::main(worker_threads = 2)]
 async fn main() -> anyhow::Result<()> {
     let args: Args = Args::parse();
-    setup_tracing(1);
+    setup_tracing(args.verbose);
 
     let mut zenoh_config = if let Some(conf_file) = &args.config {
         Config::from_file(conf_file).unwrap()
@@ -52,11 +81,19 @@ async fn main() -> anyhow::Result<()> {
     if !args.listen.is_empty() {
         zenoh_config.listen.endpoints.clone_from(&args.listen);
     }
-    info!("Using config {:?}", args.config);
-    info!("Connecting to {:?}", zenoh_config.connect.endpoints);
-    info!("Listening on {:?}", zenoh_config.listen.endpoints);
-    info!("Publishing on {:?}", args.topic);
-    info!("Starting zenoh session");
+
+    // log config
+    if let Some(config) = &args.config {
+        info!("Using zenoh config {:?}", config);
+    }
+    if !zenoh_config.connect.endpoints.is_empty() {
+        info!("Connecting to {:?}", zenoh_config.connect.endpoints);
+    }
+    if !zenoh_config.listen.endpoints.is_empty() {
+        info!("Listening on {:?}", zenoh_config.listen.endpoints);
+    }
+    info!("Publishing on topic {:?}", args.topic);
+    debug!("Starting zenoh session");
     let zenoh_session = zenoh::open(zenoh_config)
         .res()
         .await
@@ -72,7 +109,7 @@ async fn main() -> anyhow::Result<()> {
     let schema = schema_for!(InputMessage);
     info!(
         "Message schema:\n{}",
-        serde_json::to_string_pretty(&schema).unwrap()
+        serde_json::to_string_pretty(&schema)?
     );
 
     info!("Starting gamepad reader");
@@ -93,7 +130,18 @@ async fn main() -> anyhow::Result<()> {
         time: std::time::SystemTime::now().into(),
     };
 
-    loop {
+    let keep_running = Arc::new(AtomicBool::new(true));
+
+    ctrlc::set_handler({
+        let keep_running = keep_running.clone();
+        move || {
+            keep_running.store(false, Ordering::SeqCst);
+        }
+    })
+    .context("Error setting Ctrl-C handler")?;
+
+    while keep_running.load(Ordering::SeqCst) {
+        let loop_start = tokio::time::Instant::now();
         while let Some(gilrs_event) = gilrs.next_event() {
             let gamepad_id: usize = gilrs_event.id.into();
             let gamepad_data = message_data.gamepads.entry(gamepad_id).or_default();
@@ -143,7 +191,7 @@ async fn main() -> anyhow::Result<()> {
             if gamepad.is_connected() {
                 for button in Button::all_gilrs_buttons() {
                     gamepad_data
-                        .button_pressed
+                        .button_down
                         .insert(Button::from(*button), gamepad.is_pressed(*button));
                 }
 
@@ -165,168 +213,8 @@ async fn main() -> anyhow::Result<()> {
             .res()
             .await
             .map_err(ErrorWrapper::ZenohError)?;
-        tokio::time::sleep(Duration::from_millis(args.sleep_ms)).await;
+        tokio::time::sleep_until(loop_start + Duration::from_millis(args.sleep_ms)).await;
     }
-}
 
-#[derive(Debug, Deserialize, Serialize, JsonSchema)]
-pub struct InputMessage {
-    gamepads: HashMap<usize, GamepadMessage>,
-    time: DateTime<Utc>,
-}
-
-#[derive(Debug, Deserialize, Serialize, Default, JsonSchema)]
-pub struct GamepadMessage {
-    name: String,
-    button_down_event_counter: BTreeMap<Button, usize>,
-    button_up_event_counter: BTreeMap<Button, usize>,
-    button_pressed: BTreeMap<Button, bool>,
-    axis_state: BTreeMap<Axis, f32>,
-    connected: bool,
-    last_event_time: DateTime<Utc>,
-}
-
-impl GamepadMessage {}
-
-#[derive(
-    Debug, Deserialize, Serialize, PartialEq, Eq, Hash, PartialOrd, Ord, Clone, Copy, JsonSchema,
-)]
-pub enum Button {
-    South,
-    East,
-    North,
-    West,
-    C,
-    Z,
-    LeftTrigger,
-    LeftTrigger2,
-    RightTrigger,
-    RightTrigger2,
-    Select,
-    Start,
-    Mode,
-    LeftThumb,
-    RightThumb,
-    DPadUp,
-    DPadDown,
-    DPadLeft,
-    DPadRight,
-    Unknown,
-}
-
-impl Button {
-    pub fn all_gilrs_buttons() -> &'static [gilrs::ev::Button] {
-        &[
-            gilrs::ev::Button::South,
-            gilrs::ev::Button::East,
-            gilrs::ev::Button::North,
-            gilrs::ev::Button::West,
-            gilrs::ev::Button::C,
-            gilrs::ev::Button::Z,
-            gilrs::ev::Button::LeftTrigger,
-            gilrs::ev::Button::LeftTrigger2,
-            gilrs::ev::Button::RightTrigger,
-            gilrs::ev::Button::RightTrigger2,
-            gilrs::ev::Button::Select,
-            gilrs::ev::Button::Start,
-            gilrs::ev::Button::Mode,
-            gilrs::ev::Button::LeftThumb,
-            gilrs::ev::Button::RightThumb,
-            gilrs::ev::Button::DPadUp,
-            gilrs::ev::Button::DPadDown,
-            gilrs::ev::Button::DPadLeft,
-            gilrs::ev::Button::DPadRight,
-        ]
-    }
-}
-
-impl From<gilrs::ev::Button> for Button {
-    fn from(value: gilrs::ev::Button) -> Self {
-        match value {
-            gilrs::ev::Button::South => Button::South,
-            gilrs::ev::Button::East => Button::East,
-            gilrs::ev::Button::North => Button::North,
-            gilrs::ev::Button::West => Button::West,
-            gilrs::ev::Button::C => Button::C,
-            gilrs::ev::Button::Z => Button::Z,
-            gilrs::ev::Button::LeftTrigger => Button::LeftTrigger,
-            gilrs::ev::Button::LeftTrigger2 => Button::LeftTrigger2,
-            gilrs::ev::Button::RightTrigger => Button::RightTrigger,
-            gilrs::ev::Button::RightTrigger2 => Button::RightTrigger2,
-            gilrs::ev::Button::Select => Button::Select,
-            gilrs::ev::Button::Start => Button::Start,
-            gilrs::ev::Button::Mode => Button::Mode,
-            gilrs::ev::Button::LeftThumb => Button::LeftThumb,
-            gilrs::ev::Button::RightThumb => Button::RightThumb,
-            gilrs::ev::Button::DPadUp => Button::DPadUp,
-            gilrs::ev::Button::DPadDown => Button::DPadDown,
-            gilrs::ev::Button::DPadLeft => Button::DPadLeft,
-            gilrs::ev::Button::DPadRight => Button::DPadRight,
-            gilrs::ev::Button::Unknown => Button::Unknown,
-        }
-    }
-}
-
-#[derive(
-    Debug, Deserialize, Serialize, PartialEq, Eq, Hash, PartialOrd, Ord, Clone, Copy, JsonSchema,
-)]
-pub enum Axis {
-    LeftStickX,
-    LeftStickY,
-    LeftZ,
-    RightStickX,
-    RightStickY,
-    RightZ,
-    DPadX,
-    DPadY,
-    Unknown,
-}
-
-impl Axis {
-    pub fn all_axes() -> &'static [Axis] {
-        &[
-            Axis::LeftStickX,
-            Axis::LeftStickY,
-            Axis::LeftZ,
-            Axis::RightStickX,
-            Axis::RightStickY,
-            Axis::RightZ,
-            Axis::DPadX,
-            Axis::DPadY,
-        ]
-    }
-}
-
-impl From<gilrs::ev::Axis> for Axis {
-    fn from(value: gilrs::ev::Axis) -> Self {
-        match value {
-            gilrs::ev::Axis::LeftStickX => Axis::LeftStickX,
-            gilrs::ev::Axis::LeftStickY => Axis::LeftStickY,
-            gilrs::ev::Axis::LeftZ => Axis::LeftZ,
-            gilrs::ev::Axis::RightStickX => Axis::RightStickX,
-            gilrs::ev::Axis::RightStickY => Axis::RightStickY,
-            gilrs::ev::Axis::RightZ => Axis::RightZ,
-            gilrs::ev::Axis::DPadX => Axis::DPadX,
-            gilrs::ev::Axis::DPadY => Axis::DPadY,
-            gilrs::ev::Axis::Unknown => Axis::Unknown,
-        }
-    }
-}
-
-pub fn setup_tracing(verbosity_level: u8) {
-    let filter = match verbosity_level {
-        0 => tracing::level_filters::LevelFilter::WARN,
-        1 => tracing::level_filters::LevelFilter::INFO,
-        2 => tracing::level_filters::LevelFilter::DEBUG,
-        3 => tracing::level_filters::LevelFilter::TRACE,
-        _ => tracing::level_filters::LevelFilter::TRACE,
-    };
-
-    tracing_subscriber::fmt().with_max_level(filter).init();
-}
-
-#[derive(Error, Debug)]
-pub enum ErrorWrapper {
-    #[error("Zenoh error {0:?}")]
-    ZenohError(#[from] zenoh::Error),
+    Ok(())
 }
